25e315966c56ecfc73e8e4dcbb0365601dc0fdde
YukiNakayama <121099971+Y-NKYM@users.noreply.github.com>
2023-06-24 13:20:18 +0000

    [Finish] JS-2.5、README.md追加

--- app.js
+++ app.js
@@ -1,14 +1,27 @@
-let str1 = "Hello";
-let str2 = "World";
-let hello = `${str1} ${str2}`;
-alert(hello);
+// let str1 = "Hello";
+// let str2 = "World";
+// let hello = `${str1} ${str2}`;
+// alert(hello);
 
-let num1 = 5
-let num2 = 3
+// let num1 = 5;
+// let num2 = 3;
 
-let plus = `${num1} + ${num2} = ${num1 + num2}`;
-let minus = `${num1} - ${num2} = ${num1 - num2}`;
-let times = `${num1} * ${num2} = ${num1 * num2}`;
-let divide = `${num1} / ${num2} = ${num1 / num2}`;
+// let plus = `${num1} + ${num2} = ${num1 + num2}`;
+// let minus = `${num1} - ${num2} = ${num1 - num2}`;
+// let times = `${num1} * ${num2} = ${num1 * num2}`;
+// let divide = `${num1} / ${num2} = ${num1 / num2}`;
 
-alert(`${plus}\n${minus}\n${times}\n${divide}`);
+// alert(`${plus}\n${minus}\n${times}\n${divide}`);
+
+// if(true) {
+//   var name = 'Luke';
+// }
+//console.log(name);
+
+var str = "webcamp";
+function foo() {
+  console.log(str);
+  var str = "dmm webcamp";
+  console.log(str);
+}
+foo();
