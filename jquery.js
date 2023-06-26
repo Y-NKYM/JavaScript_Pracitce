1aaf3cfe2a490290aa1fe87f013773d796e468d2
YukiNakayama <121099971+Y-NKYM@users.noreply.github.com>
2023-06-26 10:10:13 +0000

    [Finish] JS-6

--- jquery.js
+++ jquery.js
@@ -4,11 +4,23 @@ $(document).ready(function () {
   //   'background-color': '#0000FF',
   //   'height': '100px'
   // });
-  $('.box1').slideDown(function(){
-      $('.box1').css({
-      'background-color': 'blue',
-      'width': '200px',
-      'height': '100px'
-      }).slideUp();
-      });
+  // $('.box1').slideDown(function(){
+  //     $('.box1').css({
+  //     'background-color': 'blue',
+  //     'width': '200px',
+  //     'height': '100px'
+  //     }).slideUp();
+  //     });
+  // $('.box1').mouseover(function(){
+  //   $('.box1').css('background', 'blue');
+  // });
+  // $('.box1').mouseout(function(){
+  //   $('.box1').css('background', 'red');
+  // });
+  $('.box1').on('click', function(){
+    $('.box1').addClass('box1-ext');
+  });
+  $('.box1').mouseout(function(){
+    $('.box1').removeClass('box1-ext');
+  });
 });
