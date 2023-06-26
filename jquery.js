b2bb69695445087e749c87b30a40ca19142e4b5d
YukiNakayama <121099971+Y-NKYM@users.noreply.github.com>
2023-06-26 09:29:35 +0000

    [Finish] JS-5

--- jquery.js
+++ jquery.js
@@ -1,3 +1,14 @@
 $(document).ready(function () {
-  $('body').html('<h1>Hello jQuery!!</h1>');
+  // $('body').html('<h1>Hello jQuery!!</h1>');
+  // $('.box1').css({
+  //   'background-color': '#0000FF',
+  //   'height': '100px'
+  // });
+  $('.box1').slideDown(function(){
+      $('.box1').css({
+      'background-color': 'blue',
+      'width': '200px',
+      'height': '100px'
+      }).slideUp();
+      });
 });
